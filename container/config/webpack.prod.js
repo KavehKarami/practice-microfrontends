b721fc21d613f0f17191bc89b31a4c063fd2f207
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-08-12 10:49:51 +0330

    Production Webpack Config for Container

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,26 +1,24 @@
+const commonConfig = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const packageJson = require("../package.json");
-const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      "*": "index.html",
-    },
-  },
+const domain = process.env.PRODUCTION_DOMAIN;
 
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
