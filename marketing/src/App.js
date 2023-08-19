3b2a754cfd97109e4079436ce9bbc24ca5be1872
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-08-19 18:29:20 +0330

    Fixing Class Name Collisions

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
+const generateClassName = createGenerateClassName({ productionPrefix: "ma" });
+
 export default function App() {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route component={Pricing} path="/pricing" exact />
