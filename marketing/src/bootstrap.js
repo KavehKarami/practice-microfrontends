7362e4ddead0f31643d25d0dcfdacf5eb3dee336
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-08-21 18:19:38 +0330

    Running Memory History in Isolation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory } from "history";
 import App from "./App";
 
-const mount = (el, { onNavigate }) => {
+const mount = (el, { onNavigate } = {}) => {
   const history = createMemoryHistory();
-  history.listen(onNavigate);
-  
+
+  if (onNavigate) history.listen(onNavigate);
+
   ReactDOM.render(<App history={history} />, el);
 };
 
