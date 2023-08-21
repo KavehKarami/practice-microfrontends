151b7efc02b224148334423cc2de0ecbadfe6e3e
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-08-21 11:25:40 +0330

    Using Memory History

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
 const generateClassName = createGenerateClassName({ productionPrefix: "ma" });
 
-export default function App() {
+export default function App({ history }) {
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route component={Pricing} path="/pricing" exact />
           <Route component={Landing} path="/" />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
